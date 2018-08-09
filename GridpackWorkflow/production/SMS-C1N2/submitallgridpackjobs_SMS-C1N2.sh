500bee71daa6b0b6ec2cfc654dd83dfaa6cfb577
Bhumika Kansal bhumika.kansal@cern.ch 07/06/2018 <bkansal@uaf-10.t2.ucsd.edu>
2018-08-09 09:08:48 -0700

    SMS-C1N2 added

--- GridpackWorkflow/production/SMS-C1N2/submitallgridpackjobs_SMS-C1N2.sh
+++ GridpackWorkflow/production/SMS-C1N2/submitallgridpackjobs_SMS-C1N2.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 SCRIPT="../../test/scripts/submitGridpackCondorJob.py"
-PROC="SMS-C1N2_"
-PARTN2="mN2-"
-PARTC1="mC1-"
+MODEL="SMS-C1N2_mChi-"
 JOBS="jobs"
-genprodir="/home/users/bkansal/SUSYsignalProduction/genproductions/"
-vomsdir="/tmp/x509up_ubkansal"
 
-for MC1 in 100 ;do #125 150 175 200 225 250 275 300 325; do 
-    for DM in 50 ;do #40 30 20 15 10 7.5 5 3 1; do 
-	MN2=`awk "BEGIN {printf \"%.2f\n\", (${MC1}-${DM})}"`
-	MN2STR=${MN2/./p}
-	MC1STR=${MC1/./p}
-	MODEL=${PROC}${PARTC1}${MC1STR}_${PARTN2}${MN2STR}
-	python ${SCRIPT} ${MODEL} --cards-dir ${JOBS}/${MODEL} --proxy ${vomsdir} --genproductions-dir ${genprodir}
-    done
+for MNLSP in 100 125 150 175 200 225 250 275 300 325 ; do
+    python ${SCRIPT} ${MODEL}${MNLSP} --cards-dir ${JOBS}/${MODEL}${MNLSP} --proxy /tmp/x509up_ubkansal --genproductions-dir "/home/users/bkansal/SUSYsignalProduction/genproductions/"
 done
